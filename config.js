35f21da5dc67aa23664d8709402a62143b2cf0b8
Kacper Lewicki <kacper.lewicki@onet.pl>
2022-09-25 19:23:04 +0200

    api news

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+
     db: "mongodb+srv://admin:Kacper100@cluster0.zfvrswx.mongodb.net/?retryWrites=true&w=majority",
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
